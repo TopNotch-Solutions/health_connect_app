68a76308ee1e881d53acc76678b10c9e936ab800
Taloshili <nghiwewelekwataloshili@gmail.com>
2025-12-03 18:41:27 +0200

    Correcting

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://192.168.11.138:4000/api';
+const API_BASE_URL = 'http://192.168.178.64:4000/api';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
